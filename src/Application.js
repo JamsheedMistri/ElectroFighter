bf8b238c2b75e302e4272c6ee9e9d7473c3abb01
Jimmy Griffith <rogueSkib@gmail.com>
2015-04-16 04:37:29 -0700

    initial commit

---
+++ src/Application.js
@@ -0,0 +1,24 @@
+import scene, effects;
+
+exports = scene(function() {
+
+  // STEP 1: Show some text!
+  var myText = scene.addText('Hello, World!');
+
+  // STEP 2: Spin the text!
+  var spinText = function () {
+    scene.animate(myText)
+      .now({ scaleX: -1 }, 1000)
+      .then({ scaleX: 1 }, 1000)
+      .then(spinText);
+  };
+  spinText();
+
+  // STEP 3: Destroy the text!
+  scene.onTouchOnce(function() {
+    effects.explode(myText);
+    myText.destroy();
+    scene.gameOver();
+  });
+
+});
